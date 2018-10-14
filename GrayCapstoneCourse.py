eda2692588381382570a80b0ed78082c535ab450
Shannon <smgrsm@users.noreply.github.com>
2018-10-14 13:31:06 -0700

    Capstone_Course_Assignment1

---
+++ GrayCapstoneCourse.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# <h1>This notebook will be mainly used for the capstone project</h1>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
